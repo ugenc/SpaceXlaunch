558d81ef61b31fba803d682527eb265400c80209
ugur <ugur.genc33@gmail.com>
2020-10-24 14:15:36 +0300

    Deployed

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(
 	})
 );
 
-/* app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "public", "index.html"));
-}); */
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Port sunucu başladı - ${PORT}`));
