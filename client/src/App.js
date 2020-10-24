c039fca313aa669c9f9a9f078f3d9a4fba40a065
ugur <ugur.genc33@gmail.com>
2020-10-24 13:57:10 +0300

    final2

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import { Launch } from "./components/Launch";
 import { Link } from "react-router-dom";
 
 const client = new ApolloClient({
-	uri: "http://localhost:5000/graphql",
+	uri: "/graphql",
 	cache: new InMemoryCache(),
 });
 
